3ea24f9ee637856ce6cbd56c3b52dfc47269f7f1
OldRabbit736 <sylvan0212@gmail.com>
2022-08-08 14:40:33 +0900

    complete: adding authorities to users

--- src/main/java/com/example/demo/security/ApplicationUserRole.java
+++ src/main/java/com/example/demo/security/ApplicationUserRole.java
@@ -1,10 +1,11 @@
 package com.example.demo.security;
 
 
-import com.google.common.collect.Sets;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
-import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static com.example.demo.security.ApplicationUserPermission.*;
 
@@ -22,4 +23,14 @@ public enum ApplicationUserRole {
     public Set<ApplicationUserPermission> getPermissions() {
         return permissions;
     }
+
+    public Set<GrantedAuthority> getGrantedAuthorities() {
+        Set<GrantedAuthority> permissions = getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+
+        permissions.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+        return permissions;
+    }
+
 }
