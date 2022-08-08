112430e976846be85c282cd789a4117cfb4903cd
OldRabbit736 <sylvan0212@gmail.com>
2022-08-08 11:39:04 +0900

    complete: password encoding with bcrypt

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -1,11 +1,21 @@
 package com.example.demo.security;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -19,4 +29,17 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic();
     }
 
+    // Bean 으로 등록된 UserDetailsService 가 없으면,
+    // 테스트 용 유저 (username: user, password: 프로그램 시작할 때 로그에 출력되는 자동 생성 비번) 자동 생성
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails annaSmithUser = User.builder()
+                .username("annaSmith")
+                .password(passwordEncoder.encode("password"))
+                .roles("STUDENT")   // ROLE_STUDENT
+                .build();
+
+        return new InMemoryUserDetailsManager(annaSmithUser);
+    }
 }
